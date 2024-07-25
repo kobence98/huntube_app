8d68cd5fc7915912faaa259be642fc39125c1beb
Kovács Bence <kovacs.bence@effectivegroup.hu>
2024-07-25 11:10:31 +0200

    ios test

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:huntube_app/app_startup_widget.dart';
+import 'package:huntube_app/presentation/theme/app_colors.dart';
 import 'package:huntube_app/the_just_matthew_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     ProviderScope(
-      child: AppStartupWidget(),
+      // child: AppStartupWidget(),
+      child: MaterialApp(
+          home: Scaffold(
+          backgroundColor: AppColors.whiteStrong,
+          body: Center(
+            child: Text(
+              'IDÁIG LEGALÁBB ELJUT',
+              style: TextStyle(color: AppColors.blackStrong),
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
